3107cf5a0671ad70d31fe25de798350635af61a4
Khao <khao@khaodoes.dev>
2025-11-05 15:14:41 +0700

    feat: sonner which is a toaster

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import { Navbar } from "./components/navbar";
 import { Footer } from "./components/footer";
+import { Toaster } from "./components/ui/sonner";
 
 export function Layout() {
   return <div className="min-h-dvh bg-background">
     <Navbar />
     <Outlet />
+    <Toaster />
     <Footer />
   </div>
 }
